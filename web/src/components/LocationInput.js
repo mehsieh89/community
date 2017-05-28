cde854e5af17ec2e7414dd8ed920c2af45adff5c
Mel hsieh <mehsieh89@gmail.com>
2017-05-28 15:17:43 -0700

    map autocompletes now and does not rerender map

--- web/src/components/LocationInput.js
+++ web/src/components/LocationInput.js
@@ -60,6 +60,7 @@ export default class LocationInput extends Component {
   handleLocationInput(location) {
     let string = location.split(' ').join('+');
     axios.get(GeoCodeURL + string + '&key=' + KEY)
+
     .then((res) => {
       let acArray = [];
       for (let i = 0; i < res.data.results.length; i++) {
