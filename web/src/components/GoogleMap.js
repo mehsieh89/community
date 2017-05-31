bf9afbb240321c4c64357d4727fff28c28884b2f
Mel hsieh <mehsieh89@gmail.com>
2017-05-31 16:11:01 -0700

    Geolocation centers to user, and added click functions for grid list for event details (#23)
    * color of markers has changed to blue
    
    * geolocation works, also added clickability to the gridlist for event details

--- web/src/components/GoogleMap.js
+++ web/src/components/GoogleMap.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker, Circle } from 'react-google-maps';
+import canUseDOM from 'can-use-dom';
+import Promise from 'bluebird';
 import axios from 'axios';
 import LocationInput from './LocationInput';
 
@@ -9,10 +11,27 @@ const style = {
   width: '100%',
 };
 
+const geolocation = (
+  canUseDOM && navigator.geolocation ?
+  navigator.geolocation :
+  ({
+    getCurrentPosition(success, failure) {
+      if (failure) {
+        console.log('Your browser does not support geolocation.');
+      }
+    },
+  })
+);
+
 class Gmap extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      center: {
+        lat: 40.758895,
+        lng: -73.985131,
+      }
+    };
     this.handleMapLoad = this.handleMapLoad.bind(this);
     this.handleMapClick = this.handleMapClick.bind(this);
     this.handleMarkerClick = this.handleMarkerClick.bind(this);
@@ -21,7 +40,6 @@ class Gmap extends Component {
   }
 
   componentDidMount() {
-    // console.log('hi from componentdidMount');
     const nextMarkers = [
       ...this.props.markers,
     ];
@@ -31,12 +49,27 @@ class Gmap extends Component {
         let newMarker = {
           position: {lat: Number(res.data[i].lat), lng: Number(res.data[i].lng)},
           defaultAnimation: 3,
-          key: Math.random(),
         };
         nextMarkers.push(newMarker);
       }
       this.props.setMarkers(nextMarkers);
       // console.log('end of component did mount', this.props.markers);
+    })
+    .then(() => {
+      let context = this;
+      geolocation.getCurrentPosition((position) => {
+        return new Promise((resolve, reject) => {
+          resolve(this.setState({
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            }
+          }));
+        })
+        .then(() => {
+          context.props.changeCenter(this.state.center);
+        });
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -55,10 +88,11 @@ class Gmap extends Component {
       {
         position: {lat: lat, lng: lng},
         defaultAnimation: 3,
-        key: Math.random(),
       },
     ];
+    console.log(this.state.center);
     this.props.setMarkers(nextMarkers);
+    // this.props._mapComponent(lat, lng);
     this.props.changeCenter({lat: lat, lng: lng});
     this.handleReverseGeoCode({lat: lat, lng: lng});
     // console.log(this.props.markers);
@@ -69,16 +103,12 @@ class Gmap extends Component {
       lat: targetMarker.position.lat,
       lng: targetMarker.position.lng
     };
-    // targetMarker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+    // this.setState({
+    //   colorChange: true,
+    // });
+    // console.log(this.state.icon);
     // this.handleReverseGeoCode(latlng);
   }
-
-  // handleMarkerClick(e) {
-  //   this.setState({
-  //     selectedPlace: props,
-  //     activeMarker: marker,
-  //   });
-  // }
 
   handleMarkerRightClick(targetMarker) {
     const nextMarkers = this.props.markers.filter(marker => marker !== targetMarker);
@@ -97,19 +127,21 @@ class Gmap extends Component {
 
   render () {
     // console.log('withGmap', this.props.markers);
+    // const icon = this.state.colorChange ? 'http://maps.google.com/mapfiles/ms/icons/green-dot.png' : 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
     const Map = withGoogleMap(props => (
       <GoogleMap
         ref={props.onMapLoad}
-        zoom={0}
-        center={this.props.googleMap}
+        zoom={14}
+        center={this.props.center}
         onClick={props.onMapClick}
         >
-        {props.markers.map((marker) => (
+        {props.markers.map((marker, index) => (
           <Marker
           {...marker}
           onClick={() => props.onMarkerClick(marker)}
           onRightClick={() => props.onMarkerRightClick(marker)}
-          // setIcon={() => props.setIcon(url)}
+          icon={'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'}
+          key={index}
           >
           </Marker>
         ))}
