78a6db670644c78213232757aba890968f802264
Ramsha A. Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-06-07 11:40:18 -0700

    Implement logout functionality and link to user profile (#35)
    * Implement a drawer
    
    * Implement logout functionality and link to user profile
    
    * Remove unnecessary console logs

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,9 +1,39 @@
-import { AppBar } from 'material-ui';
+import { AppBar, Drawer, MenuItem } from 'material-ui';
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class Header extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      open: false
+    };
+
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handleEditProfile = this.handleEditProfile.bind(this);
+  }
+
+  toggleDrawer() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+  handleClose(event) {
+    this.setState({
+      open: false
+    });
+  }
+
+  handleEditProfile() {
+    axios.get('/profile');
+  }
+
+  handleLogout() {
+    axios.get('/logout');
   }
 
   render () {
@@ -13,7 +43,31 @@ class Header extends Component {
           title="Community"
           style={styles.theme}
           titleStyle={styles.title}
+          onLeftIconButtonTouchTap={this.toggleDrawer}
         />
+          <Drawer
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <AppBar
+              title="Community"
+              showMenuIconButton={false}
+              style={styles.theme}
+              titleStyle={styles.title}
+            />
+            <MenuItem
+              onTouchTap={this.handleClose}
+              onTouchTap={this.handleEditProfile}
+              primaryText="User Profile"
+              href={'/profile'}
+            />
+            <MenuItem
+              onTouchTap={this.handleClose}
+              onTouchTap={this.handleLogout}
+              primaryText="Logout"
+              href={'/login'}
+            />
+          </Drawer>
       </div>
     );
   }
